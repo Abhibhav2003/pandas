e549a80f0bd424369a86e3cfab5c22b2254b2288
Abhibhav Sharma <abhibhavsharma50@gmail.com>
2025-03-08 21:30:41 +0530

    Updated

--- pandas/tests/groupby/methods/test_describe.py
+++ pandas/tests/groupby/methods/test_describe.py
@@ -198,21 +198,23 @@ def test_describe_with_duplicate_output_column_names(as_index, keys):
 def test_describe_duplicate_columns():
     # GH#50806
     df = DataFrame([[0, 1, 2, 3]])
-    df.columns = [0, 1, 2, 0]
+    df.columns = [0, 1, 2, 0]  # Duplicate column names
     gb = df.groupby(df[1])
     result = gb.describe(percentiles=[])
 
-    columns = ["count", "mean", "std", "min", "50%", "max"]
+    columns = ["count", "mean", "std", "min", "max"]
     frames = [
-        DataFrame([[1.0, val, np.nan, val, val, val]], index=[1], columns=columns)
-        for val in (0.0, 2.0, 3.0)
+        DataFrame([[1.0, 0.0, np.nan, 0.0, 0.0]], index=[1], columns=columns),
+        DataFrame([[1.0, 2.0, np.nan, 2.0, 2.0]], index=[1], columns=columns),
+        DataFrame([[1.0, 3.0, np.nan, 3.0, 3.0]], index=[1], columns=columns),
     ]
+
     expected = pd.concat(frames, axis=1)
-    expected.columns = MultiIndex(
-        levels=[[0, 2], columns],
-        codes=[6 * [0] + 6 * [1] + 6 * [0], 3 * list(range(6))],
-    )
+
+    expected.columns = result.columns
+
     expected.index.names = [1]
+
     tm.assert_frame_equal(result, expected)
 
 
